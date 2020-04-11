b704e1c7db4d469c1e6f2e22241553ea2598206e
duch0416 <mikolajduchewicz@gmail.com>
2020-04-11 18:30:12 +0200

    delete useEvolutuions imgs hook and add universal getEvolutionsImgs function

--- src/singlePokemon/actions/getEvolutionImgs.tsx
+++ src/singlePokemon/actions/getEvolutionImgs.tsx
@@ -4,13 +4,18 @@ export const getEvolutionImgs = async (pokemonName: string) => {
     const data = await getEvolutionChain(pokemonName);
     
     const firstForm = data.chain.species.name;
-    const secondForm =  data.chain.evolves_to[0].species.name;
-    const thirdForm = data.chain.evolves_to[0].evolves_to[0].species.name;
+    const evolutionsNames = [firstForm]
+    if(data.chain.evolves_to.length != 0) {
+      const secondForm =  data.chain.evolves_to[0].species.name;
+      evolutionsNames.push(secondForm)
+      if(data.chain.evolves_to[0].evolves_to.length != 0){
+        const thirdForm = data.chain.evolves_to[0].evolves_to[0].species.name;
+        evolutionsNames.push(thirdForm)
+      }
+    }
     
-    
-    const evolutionsNames = [firstForm, secondForm, thirdForm]
     console.log(data)
-
+    
    const imgs = await Promise.all(evolutionsNames.map(async (pokemonName: string) => {
         const pokemon = await getSinglePokemon(pokemonName)
         return   pokemon.sprites.front_default
